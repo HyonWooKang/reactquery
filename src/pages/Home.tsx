d75a2c492ea4c2a605e6e0eb96d06b89a253cd4a
HyonWooKang <87benzenes@gmail.com>
2025-11-13 21:08:20 +0900

    Home api 변경 및 화면 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,59 @@
 import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const fetchPosts = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+type Recipe = {
+  id: number;
+  name: string;
+  instructions: string[];
+  // 필요한 만큼 더 추가 가능
+};
+
+type RecipeResponse = {
+  recipes: Recipe[];
+  total: number;
+  skip: number;
+  limit: number;
+};
+
+const fetchRecipes = async (): Promise<RecipeResponse> => {
+  // const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const res = await fetch("https://dummyjson.com/recipes");
   if (!res.ok) throw Error("failed to fetch");
   return res.json();
 };
 
 export default function Home() {
   const { data, isLoading, error } = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPosts,
+    queryKey: ["recipes"],
+    queryFn: fetchRecipes,
   });
 
-  console.log("data", data);
+  // console.log("data", data);
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error occurred</p>;
+
+  const recipes = data?.recipes ?? [];
 
   return (
     <>
       <h1>Post List</h1>
       <ul>
-        {data?.slice(0, 10).map((post: any) => (
-          <li key={post.id}>
-            <Link to={`/detail/${post.id}`}>{post.title}</Link>
-          </li>
-        ))}
-        {/* {data.map((post: any) => (
+        {/* {data?.slice(0, 10).map((post: any) => (
           <li key={post.id}>
             <Link to={`/detail/${post.id}`}>{post.title}</Link>
           </li>
         ))} */}
+        {recipes.length > 0 ? (
+          recipes.map((recipes) => (
+            <li key={recipes.id}>
+              <Link to={`/detail/${recipes.id}`}>{recipes.name}</Link>
+            </li>
+          ))
+        ) : (
+          <p>no data</p>
+        )}
       </ul>
     </>
   );
