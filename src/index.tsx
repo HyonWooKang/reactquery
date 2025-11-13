9b9a527e2191cec6fd282b9559cb6be7a3881dfe
Hyon Woo Kang <87benzenes@gmail.com>
2025-11-13 16:06:07 +0900

    React Query 설정 및 Home 화면 작성

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import route from "./route/Rooutes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={route} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={route} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
+
+const fetchPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) throw Error("failed to fetch");
+  return res.json();
+};
+
 export default function Home() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  console.log("data", data);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error occurred</p>;
+
   return (
     <>
-      <div>
-        <h2>Welcome, it's Home</h2>
-      </div>
-      <div>
-        <section>
-          <div>
-            <ul>
-              <ol>hi</ol>
-            </ul>
-          </div>
-        </section>
-      </div>
+      <h1>Post List</h1>
+      <ul>
+        {data?.slice(0, 10).map((post: any) => (
+          <li key={post.id}>
+            <Link to={`/detail/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+        {/* {data.map((post: any) => (
+          <li key={post.id}>
+            <Link to={`/detail/${post.id}`}>{post.title}</Link>
+          </li>
+        ))} */}
+      </ul>
     </>
   );
 }
