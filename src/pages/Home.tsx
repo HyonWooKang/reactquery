9938f67b6d89a9374e4557a7d16825e86d31d6d4
HyonWooKang <87benzenes@gmail.com>
2025-11-19 00:09:09 +0900

    Home 화면에 필요한 타입 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,6 +23,16 @@ export default function Home() {
   const { data, isLoading, error } = useQuery<PokemonListResponse>({
     queryKey: ["pokemon-list"],
     queryFn: fetchPokemonList,
+    // query options
+    enabled: true, // 조건부 동작
+    staleTime: 0, // refetch 주기 // milisecond
+    gcTime: 5 * 60 * 1000, // 캐시 삭제 시간 - 5분
+    refetchOnWindowFocus: true, // 해당 화면에 포커스 오면 refetch
+    refetchOnReconnect: true, // 네트워크 끊기면 refetch
+    refetchInterval: false, // 주기적으로 데이터 refetch
+    retry: 3, // 실패시 재시도 횟수
+    select: (data) => data, // 응답값을 내가 원하는대로 변환 ex).slice(0, 5)
+    initialData: undefined, // 초기값 지정
   });
 
   if (isLoading) return <p>Loading...</p>;
